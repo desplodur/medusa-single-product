54360943f0419bd5c42088d6a67cae3d1fc071ca
Laurenz Rudolph <Laurenz.Rudolph@t-online.de>
2023-12-22 18:23:25 +0100

    create mobile header

--- src/modules/mobile-menu/components/container/index.tsx
+++ src/modules/mobile-menu/components/container/index.tsx
@@ -1,47 +1,51 @@
-import { Dialog, Transition } from "@headlessui/react"
 import { useMobileMenu } from "@lib/context/mobile-menu-context"
-import { Fragment } from "react"
+import { motion } from "framer-motion"
 
 type ContainerProps = {
   children: React.ReactNode
 }
 
 const Container = ({ children }: ContainerProps) => {
-  const { state, close } = useMobileMenu()
-  return (
-    <Transition.Root show={state} as={Fragment}>
-      <Dialog as="div" className="fixed inset-0 flex z-50" onClose={close}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-in-out duration-500"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in-out duration-500"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <Dialog.Overlay className="absolute inset-0 bg-gray-700 bg-opacity-75 transition-opacity backdrop-blur-sm" />
-        </Transition.Child>
+  const { state, close, toggle } = useMobileMenu()
 
-        <Transition.Child
-          as={Fragment}
-          enter="transition ease-in-out duration-500 transform"
-          enterFrom="-translate-x-full"
-          enterTo="translate-x-0"
-          leave="transition ease-in-out duration-500 transform"
-          leaveFrom="translate-x-0"
-          leaveTo="-translate-x-full"
-        >
-          <div className="absolute inset-0 overflow-hidden trans">
-            <div className="pointer-events-none fixed inset-y-0 right-left flex max-w-full">
-              <div className="relative w-screen pointer-events-auto bg-white text-gray-900 flex flex-col overflow-y-auto">
-                {children}
-              </div>
-            </div>
-          </div>
-        </Transition.Child>
-      </Dialog>
-    </Transition.Root>
+  const menu_variants = {
+    open: {
+      height: "95vh",
+      type: "spring",
+    },
+    closed: {
+      height: "15vh",
+      type: "spring",
+    },
+  }
+  const link_variants = {
+    open: {
+      justifyContent: "center",
+    },
+    closed: {
+      justifyContent: "flex-start",
+    },
+  }
+
+  return (
+    <motion.div
+      animate={state ? "open" : "closed"}
+      onClick={toggle}
+      className="fixed small:hidden bottom-0 left-0 right-0 z-50 w-full"
+      variants={menu_variants}
+    >
+      <div className="absolute left-0 bottom-[90%] border-primary border-b-[0.8rem] w-28 border-r-[2rem] border-r-transparent bg-transparent flex items-center justify-center" />
+      <div className="absolute bottom-[90%] left-1/2 transform -translate-x-1/2 -translate-y-[5px]">
+        <div className="w-12 h-1 bg-primary animate-bounce" />
+      </div>
+      <div className="absolute right-0 bottom-[90%] border-primary border-b-[0.8rem] w-28 border-l-[2rem] border-l-transparent bg-transparent flex items-center justify-center" />
+      <motion.div
+        className="absolute left-0 right-0 bottom-0 bg-primary h-[90%] flex flex-col items-center py-6"
+        variants={link_variants}
+      >
+        {state ? children : <h1 className="text-white">Menu</h1>}
+      </motion.div>
+    </motion.div>
   )
 }
 
